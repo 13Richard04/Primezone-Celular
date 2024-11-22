511142a6929120430a7d6cb0808377f63c443bfc
Bruvini2006 <bruvini06@gmail.com>
2024-11-22 08:53:51 -0300

    paginas funcionais

---
+++ Prime-Mobile/DB/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Sua configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyColq8nh741FW-DeAVRdc77ZcC9FP-Blp8",
+    authDomain: "teste-denovo-1d43c.firebaseapp.com",
+    projectId: "teste-denovo-1d43c",
+    storageBucket: "teste-denovo-1d43c.appspot.com",
+    messagingSenderId: "384646857934",
+    appId: "1:384646857934:web:70f54a0c677d602d03fc9f",
+    measurementId: "G-E9FET4BYVZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
